37433ea32a6fcaa76166a746a4d7d5c4e46c86a4
Cristian Franco <cfrancobedoya@gmail.com>
2025-04-21 23:59:13 -0500

    Add Tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/common/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0056b3',
+        'primary-dark': '#004494',
+        'primary-light': '#a3c4e7',
+      },
+    },
+  },
+  plugins: [],
+}; 
